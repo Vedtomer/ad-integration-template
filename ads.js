4c129bd82d8d35423e5490f2b77741747f1845bd
Ved Tomer <vedtomer@gmail.com>
2025-03-26 01:57:44 +0530

    fix

--- ads.js
+++ ads.js
@@ -116,6 +116,7 @@ class AdSystem {
     const isPIDValid = await PIDValidator.validatePID();
     
     if (!isPIDValid) {
+      alert("PID validation failed. Ad system will not initialize");
       console.error("PID validation failed. Ad system will not initialize.");
       return;
     }
